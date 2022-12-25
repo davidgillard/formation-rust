c1df4a1ba77a90d76f852fc34eb68eb7eef60401
David Gillard <ddavid.ggillard@protonmail.com>
2022-12-25 18:10:06 +0100

    changement de nommage du rep

---
+++ 01-hello_world/target/debug/deps/hello_world-18f3c2a0052c492d.d
@@ -0,0 +1,5 @@
+/home/david/Documents/projets/rust/formation-rust/hello_world/target/debug/deps/hello_world-18f3c2a0052c492d: src/main.rs
+
+/home/david/Documents/projets/rust/formation-rust/hello_world/target/debug/deps/hello_world-18f3c2a0052c492d.d: src/main.rs
+
+src/main.rs:
---
+++ 01-hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/david/Documents/projets/rust/formation-rust/hello_world/target/debug/hello_world: /home/david/Documents/projets/rust/formation-rust/hello_world/src/main.rs
