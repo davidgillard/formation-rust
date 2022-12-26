598ed7abf9de45c5d7a5707b6661a7a05394d903
David Gillard <ddavid.ggillard@protonmail.com>
2022-12-26 20:10:43 +0100

    Un Bonjour en plusieurs langues

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    greet_world();
+}
+
+fn greet_world() {
+    let russian_language = "Всем привет";
+    let french_language = "Bonjour tout le monde";
+    let english_language = "Hello, world";
+    let japan_language = "ハロー・ワールド";
+    let chinese_language = "大家好";
+    let regions = [
+        russian_language,
+        french_language,
+        english_language,
+        japan_language,
+        chinese_language,
+    ];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
 }
